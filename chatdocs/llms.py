b9512161cc014e58a88e1a90f01caf54a1ef1737
aka. K00B404 aka. Bnonymous <goldenkooy@gmail.com>
2024-05-11 01:48:03 +0200

    Update llms.py

--- chatdocs/llms.py
+++ chatdocs/llms.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from langchain_community.llms import CTransformers, HuggingFacePipeline, OpenAI
-from langchain_community.base import LLM
+from langchain_community.llms import LLM
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 from .utils import merge
